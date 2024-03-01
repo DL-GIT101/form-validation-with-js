42cb172e5b987d121f76675139d9d11c2517f9a4
donlHenard <pastor.git101@gmail.com>
2024-03-01 15:26:50 +0800

    Add the funciton for creating form

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
+import { createConfirmPasswordInput, createEmailInput, createForm, createPasswordInput, createSelect, createTextInput } from './form';
 import './style.css';
 
-alert("test");
+const formValidation = () => {
+    
+const body = document.querySelector('body');
+
+    const form = createForm();
+    
+        const emailInput = createEmailInput();
+
+        const countries = ["Philippines","Japan","South Korea"];
+        const select = createSelect("Country",countries);
+
+        const zipInput = createTextInput("Zip");
+
+        const passwordInput = createPasswordInput();
+
+        const confirmPasswordInput = createConfirmPasswordInput();
+    
+    form.append(emailInput,select,zipInput,passwordInput,confirmPasswordInput);
+    
+body.append(form);
+}
+
+formValidation();
