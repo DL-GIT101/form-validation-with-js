e74145b35d826d9ab19fa274b59524902ffac373
donlHenard <pastor.git101@gmail.com>
2024-03-05 16:31:39 +0800

    get email via queryselector add validated class

--- src/formValidation.js
+++ src/formValidation.js
@@ -1,21 +1,23 @@
 const validateEmail = () => {
-    const email = document.querySelector('#email');
-    const value = email.value;
+    const input = document.querySelector('#email');
+    const value = input.value;
 
-    const container = email.closest('.input-container');
-    const message = container.querySelector('.message');
-
+    const message = email.nextElementSibling;
+    
     const regex = /[A-Za-z0-9._+\-']+@[A-Za-z0-9.\-]+\.[A-Za-z]{2,}/;
    
     if(value === ''){
-        email.setCustomValidity("Please Fill out this field");
+        input.setCustomValidity("Please Fill out this field");
     }
     else if(!value.match(regex)){
-        email.setCustomValidity("Wrong Format");
-        message.textContent = "Example: email@gmail.com";
-    }else {
-        email.setCustomValidity("");
+        message.textContent = "Ex. email@domain.com";
     }
+    else {
+        input.setCustomValidity("");
+        message.textContent = '';
+    }
+
+    input.classList.add('validated');
 
 }
 
@@ -24,6 +26,4 @@ const validateForm = () => {
 }
 
 
-export {validateEmail,
-        validateForm
-};
+export { validateForm };
