e38f2cd259dc805c610009c77f1cae483502efca
donlHenard <pastor.git101@gmail.com>
2024-03-06 16:04:06 +0800

    Add validation on zip

--- src/formValidation.js
+++ src/formValidation.js
@@ -8,8 +8,9 @@ const validateEmail = () => {
    
     if(value === ''){
         input.setCustomValidity("Please Fill out this field");
+        message.textContent = "Please Fill out this field";
     }
-    else if(!value.match(regex)){
+    else if(!regex.test(value)){
         message.textContent = "Ex. email@domain.com";
     }
     else {
@@ -23,10 +24,51 @@ const validateEmail = () => {
 
 const validateZip = () => {
     
+    const countries = [ { 
+        "Country": "Philippines",
+        "ISO": "PH",
+        "Format": "NNNN",
+        "Regex": "^\\d{4}$"
+    },{
+        "Country": "Japan",
+        "ISO": "JP",
+        "Format": "NNNNNNN (NNN-NNNN)",
+        "Regex": "^\\d{7}\\s\\(\\d{3}-\\d{4}\\)$"
+    },{
+        "Country": "South Korea",
+        "ISO": "KR",
+        "Format": "NNNNN",
+        "Regex": "^\\d{5}$"
+    } ];
+
+    const countrySelected = document.querySelector('#country').value;
+
+    const zip = document.querySelector('#zip');
+    const message = zip.nextElementSibling;
+
+    let pattern;
+    let format;
+    countries.forEach(country => {
+        if(country.ISO == countrySelected){
+            pattern = new RegExp(country.Regex);
+            format = country.Format;
+        }
+    });
+
+    if(!pattern.test(zip.value)){
+        zip.setCustomValidity("Follow the format");
+        message.textContent = format;
+    }else {
+        zip.setCustomValidity("");
+        message.textContent = '';
+    }
+
+    zip.classList.add('validated');
 }
 
 const validateForm = () => {
     validateEmail();
+    validateZip();
 }
 
 
